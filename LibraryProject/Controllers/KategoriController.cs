c5aa2ce34ade8b6a0e739a843c074f888d04cef1
zehrasbr <zehraisbr@gmail.com>
2023-10-19 18:40:41 +0300

    category

--- LibraryProject/Controllers/KategoriController.cs
+++ LibraryProject/Controllers/KategoriController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryProject.Models.Entity;
 
 namespace LibraryProject.Controllers
 {
     public class KategoriController : Controller
     {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
         }
     }
 }
---
+++ LibraryProject/Controllers/KitapController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryProject.Controllers
+{
+    public class KitapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
